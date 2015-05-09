dbd3464d004252fb2b4b08b05388bb7b37dea62c
cbuu <ioricbuu@gmail.com>
2015-05-10 02:16:13 +0800

    create a simple loginscene

--- src/com/example/highnight/LoginActivity.java
+++ src/com/example/highnight/LoginActivity.java
@@ -1,36 +1,56 @@
 package com.example.highnight;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 
 
 public class LoginActivity extends Activity {
 
+	private TextView registerButton = null;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        initView();
+
     }
 
+    private void initView(){
+    	registerButton = (TextView)findViewById(R.id.button_to_register);
+    	
+    	registerButton.setOnTouchListener(new View.OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				
+				int action = arg1.getAction();
+				
+				Log.d("test", ""+action);
+				
+				if (action == MotionEvent.ACTION_DOWN) {
+					registerButton.setTextColor(Color.GREEN);
+				}
+				
+				if (action == MotionEvent.ACTION_UP) {
+					registerButton.setTextColor(Color.WHITE);
+					startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+				}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+				
+				return true;
+			}
+		});
     }
 }
